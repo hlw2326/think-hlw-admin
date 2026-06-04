280eaa2a7b8baa1c8869f01280255d0404893b62
黄先生 <1608626145@qq.com>
2026-06-04 12:39:27 +0800

    新增更新

--- uni/src/App.vue
+++ uni/src/App.vue
@@ -8,9 +8,22 @@ onLaunch((options: any) => {
     applyLaunch(options);
     console.log("App Launch", options);
 });
+const updateManager = uni.getUpdateManager();
+
 onShow((options: any) => {
     applyLaunch(options);
     console.log("App Show", options);
+    updateManager.onUpdateReady(() => {
+        uni.showModal({
+            title: '更新提示',
+            content: '新版本已经准备好，是否重启应用？',
+            success: (res: any) => {
+                if (res.confirm) {
+                    updateManager.applyUpdate();
+                }
+            }
+        });
+    });
 });
 onHide(() => {
     console.log("App Hide");
